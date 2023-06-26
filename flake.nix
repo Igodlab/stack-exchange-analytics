097a53dbf0061c6a83e9d9881a95f644e0954e52
igodlab <igodlab@gmail.com>
2023-06-25 23:57:29 -0700

    nix flakes python ref fixed, pointed to python in my own system now it points to nix store

--- flake.nix
+++ flake.nix
@@ -1,21 +1,29 @@
 {
   description = "Starting to try good Ops procedures w/ Nix";
 
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-22.05";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
 
   outputs = { self, nixpkgs }:
-  let 
-    pkgs = import nixpkgs {
-      system = "x86_64-linux";
-    };
-  in
-    { 
+    let
+      pkgs = import nixpkgs {
+        system = "x86_64-linux";
+      };
+      pythonPackages = pkgs.python311Packages;
+      pyPkgs = pythonPackages: with pythonPackages; [
+        pandas
+        matplotlib
+        numpy
+        plotly
+        seaborn
+      ];
+    in
+    {
       devShells.x86_64-linux = {
         default = pkgs.mkShell {
-          buildInputs = [
-            pkgs.python311
-            pkgs.streamlit
-            pkgs.cowsay
+          buildInputs = [ 
+            (pkgs.python3.withPackages pyPkgs)
+            pkgs.streamlit 
+            pkgs.cowsay 
           ];
         };
       };
